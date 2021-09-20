137f53effbdcb606828f7e9559b62bab68843810
Dawid Poznański <dawid.poznanski@outlook.com>
2021-09-20 21:49:57 +0200

    add trips filter by id and code in

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
+import { AnimatedSwitch } from 'react-router-transition';
 import PropTypes from 'prop-types';
+import styles from './App.scss'
 
 import MainLayout from './components/layout/MainLayout/MainLayout';
 
@@ -41,17 +43,22 @@ class App extends React.Component {
     return (
       <BrowserRouter>
         <MainLayout>
-          <Switch>
+          <AnimatedSwitch
+          atEnter={{ opacity: 0 }}
+          atLeave={{ opacity: 0 }}
+          atActive={{ opacity: 1 }}
+          className={styles.switchWrapper}
+          >
             <Route exact path='/' component={Home} />
             <Route exact path='/trips' component={Trips} />
             {/* TODO - add more routes for other views */}
-            <Route exact path='/trip' component={Trip} />
+            <Route exact path='/trip/:id' component={Trip} />
             <Route exact path='/regions' component={Regions} />
-            <Route exact path='/country' component={Country} />
+            <Route exact path='/country/:id' component={Country} />
             <Route exact path='/countries' component={Countries} />
             <Route exact path='/info' component={Info} />
             <Route path='*' component={NotFound} />
-          </Switch>
+          </AnimatedSwitch>
         </MainLayout>
       </BrowserRouter>
     );
